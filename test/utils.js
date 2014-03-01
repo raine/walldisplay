7493bc4f37a36d46a53df513aa6df8b0ccd71452
Raine Virta <raine.virta@gmail.com>
2014-03-01 13:03:45 +0200

    utils: test that keys have no dots

--- test/utils.js
+++ test/utils.js
@@ -1,7 +1,8 @@
 var should = require('should')
   , utils = require('../lib/utils')
   , preparePayload = utils.preparePayload
-  , fs = require('fs');
+  , fs = require('fs')
+  , _ = require('lodash');
 
 var payloadJSON = fs.readFileSync('test/fixtures/payload.json', 'utf8');
 
@@ -13,11 +14,29 @@ describe('utils', function() {
     });
 
     it('should remove mongo incompatible key in config from payload', function() {
-      payload.config.should.not.have.property('.result');
+      payload['config'].should.not.have.property('.result');
     });
 
     it('should remove matrix from payload', function() {
       payload.should.not.have.property('matrix');
     });
+
+    it('should have no keys with a dot', function() {
+      _.any(keys(payload), hasDot).should.be.false;
+    });
   });
 });
+
+function keys(obj) {
+  return _.reduce(obj, function(mem, val, key) {
+    if (_.isPlainObject(val)) {
+      return mem.concat(keys(val));
+    } else {
+      return mem.concat(key);
+    }
+  }, []);
+};
+
+function hasDot(obj) {
+  return _.contains(obj, '.');
+}
